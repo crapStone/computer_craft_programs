2bc293c4bb75d8b1e61f5717682d034ea4d74511
crapStone <wewr.mc@gmail.com>
2020-06-15 04:45:49 +0200

    updated install script to use github instead of pastebin

--- install.lua
+++ install.lua
@@ -1,6 +1,21 @@
-shell.run("rom/programs/http/pastebin get NSrf5fWY craplib.lua")
-shell.run("rom/programs/http/pastebin get W8b3qbYf forest.lua")
-shell.run("rom/programs/http/pastebin get QGBxP7SZ quarry.lua")
-shell.run("rom/programs/http/pastebin get 0qCfYC9M stripMine.lua")
-shell.run("rom/programs/http/pastebin get HKDGV344 tree.lua")
-shell.run("rom/programs/http/pastebin get AiYbPpam update.lua")
+-- https://pastebin.com/HKckLDnp
+-- pastebin run HKckLDnp
+
+local scripts = http.get("https://raw.githubusercontent.com/crapStone/computer_craft_programs/master/script_list").readAll()
+
+print("=======================")
+print(" installing scripts...")
+print("=======================")
+
+for script in string.gmatch(scripts, "%a+") do
+    local url = string.format("https://raw.githubusercontent.com/crapStone/computer_craft_programs/master/%s.lua", script)
+    local content = http.get(url)
+
+    local fh = fs.open(string.format("%s.lua", script), "w")
+
+    fh.write(content.readAll())
+
+    fh.close()
+
+    print(string.format("installed %s", script))
+end
